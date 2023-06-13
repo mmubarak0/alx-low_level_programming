86778c720d820471865e9029d09abb5d6b1d44d6
mohammed mubarak <modymu9@gmail.com>
2023-06-13 23:04:54 +0200

    added 10d 100-argstrostr advanced task - file: 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,5 +12,32 @@
   */
 char *argstostr(int ac, char **av)
 {
+	int k = 0, i, j, n = 0;
+	char *cop;
+	char *result;
 
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		cop = av[i];
+		j = 0;
+		while (cop[j++] != '\0')
+			k++;
+	}
+	result = malloc(sizeof(char) * k + ac);
+	if (result == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		cop = av[i];
+		j = 0;
+		while (cop[j] != '\0')
+		{
+			result[n++] = cop[j++];
+		}
+		result[n++] = '\n';
+	}
+	result[n] = '\0';
+	return (result);
 }
