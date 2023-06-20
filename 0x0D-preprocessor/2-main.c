b7262f96db53a8a816becca13c30267a93ec56f1
mohammed mubarak <modymu9@gmail.com>
2023-06-20 22:32:27 +0200

    added 0x0D tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Return: 0 always
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+#define ABS(x) (((x) < 0) ? (-(unsigned int)(x)) : ((unsigned int)(x)))
+
+#endif /* FUNC_H */
