abea8befd0c11630c11f3243565301d98d662a41
Mohammed Mubarak <modymu9@gmail.com>
2023-06-21 07:51:55 +0200

    fix

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef FUNC_H
 #define FUNC_H
 
-#define ABS(x) (((x) < 0) ? (-(unsigned int)(x)) : ((unsigned int)(x)))
+#define ABS(x) (((x) < 0) ? (-(double)(x)) : ((double)(x)))
 
 #endif /* FUNC_H */
