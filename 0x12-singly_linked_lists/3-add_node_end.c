309b80c6d36dccd2a6b5c6355800da9152edb987
Mohammed Mubarak <modymu9@gmail.com>
2023-07-01 22:30:22 +0200

    small fix

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -28,9 +28,14 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new = malloc(sizeof(list_t));
 	list_t *p;
 
+	if (new == NULL)
+		return (NULL);
 	new->str = strdup(str);
 	if (new->str == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 	new->len = _strlen(str);
 	if (*head == NULL)
 		*head = new;
