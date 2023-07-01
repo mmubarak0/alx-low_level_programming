13eb2b5111dcc91ff8cf60e674d491035dd3d6d8
Mohammed Mubarak <modymu9@gmail.com>
2023-07-01 22:32:30 +0200

    hot fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -31,7 +31,10 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	new->str = strdup(str);
 	if (new->str == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 	new->len = _strlen(str);
 	new->next = *head;
 	*head = new;
