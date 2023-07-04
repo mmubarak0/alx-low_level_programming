4c6ce4610aa2f4d6740528c7466156dcbaf9d6c4
Mohammed Mubarak <modymu9@gmail.com>
2023-07-04 22:51:07 +0200

    hot fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *p = *head;
 
+	if (head == NULL)
+		return;
 	while (p != NULL)
 	{
 		p = p->next;
 		free(*head);
 		*head = p;
 	}
-	head = NULL;
+	*head = NULL;
 }
