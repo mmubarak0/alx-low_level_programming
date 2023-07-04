6c9b7afee9d418115bb498766121999309d8b3e0
Mohammed Mubarak <modymu9@gmail.com>
2023-07-04 15:17:58 +0200

    hot fix

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,6 +3,11 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+/**
+  * listint_len - get length of the linked list.
+  * @h: head of the list.
+  * Return: size of the list
+  */
 size_t listint_len(const listint_t *h)
 {
 	const listint_t *p = h;
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,6 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new->next = *head;
+		*head = new;
 		return (new);
 	}
 	for (i = 0; p != NULL && i < (idx - 1); i++)
