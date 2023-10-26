02ced797778ee0cc649d70d2e2e706e423556b42
Mohammed Mubarak <modymu9@gmail.com>
2023-10-26 05:06:27 +0200

    added checks for boundry values

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,13 +10,13 @@ def island_perimeter(grid):
         for idy, column in enumerate(row):
             nc = 4
             if column == 1:
-                if grid[idx - 1][idy] == 1:
+                if idx > 0 and grid[idx - 1][idy] == 1:
                     nc -= 1
-                if grid[idx][idy - 1] == 1:
+                if idy > 0 and grid[idx][idy - 1] == 1:
                     nc -= 1
-                if grid[idx][idy + 1] == 1:
+                if idy < len(grid) - 1 and grid[idx][idy + 1] == 1:
                     nc -= 1
-                if grid[idx + 1][idy] == 1:
+                if idx < len(grid) - 1 and grid[idx + 1][idy] == 1:
                     nc -= 1
                 perimeter += nc
     return perimeter
